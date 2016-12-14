016e2bd438bfbbb88abff479f6c4f8c94531af10
Noah Manneschmidt <noah@manneschmidt.net>
2016-12-14 00:20:16 -0800

    Expanding submit form

--- client-src/components/lists/SubmitForm.tsx
+++ client-src/components/lists/SubmitForm.tsx
@@ -14,24 +14,48 @@ export interface ConnectedDispatch {
 
 type CombinedTypes = UsersListProps & ConnectedProps;
 
-export class SubmitForm extends React.Component<CombinedTypes, void> {
-    private input: HTMLInputElement;
+interface SubmitFormState {
+    expanded?: boolean;
+}
 
+export class SubmitForm extends React.Component<CombinedTypes, SubmitFormState> {
+    constructor(props: CombinedTypes) {
+        super(props);
+        this.state = {};
+    }
+
+    private input: HTMLInputElement;
     render() {
-        return <form id="SubmitForm" onSubmit={this.onSubmitList}>
-            <label>
-                Join the fun:<br />
-                <input
-                    required
-                    type="url"
-                    title="Submit a link to your top 10 prediction list on giantbomb.com"
-                    placeholder="http://www.giantbomb.com/profile/cathadan/lists/maybe-the-best-of-2009/357929/"
-                    name="listUrl"
-                    ref={e => this.input = e}
-                />
-            </label>
-            <button>Submit my list!</button>
-        </form>;
+        let contents: JSX.Element;
+        if (this.state.expanded) {
+            contents = <form id="SubmitForm" onSubmit={this.onSubmitList}>
+                <label>
+                    OK, where's your list?<br />
+                    <input
+                        autoFocus
+                        required
+                        type="url"
+                        title="Submit a link to your top 10 prediction list on giantbomb.com"
+                        placeholder="http://www.giantbomb.com/profile/cathadan/lists/maybe-the-best-of-2009/357929/"
+                        name="listUrl"
+                        ref={e => this.input = e}
+                    />
+                </label>
+                <button>I'm in!</button>
+            </form>;
+        } else {
+            contents = <a href="#" onClick={this.expand}>Wanna join the fun?</a>;
+        }
+        return <div id="SubmitForm">
+            {contents}
+        </div>;
+    }
+
+    private expand = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        this.setState({
+            expanded: true,
+        });
     }
 
     private onSubmitList = (e: React.FormEvent<HTMLFormElement>) => {
