ee4d2d989b9655616766b224bfe426ae473f1ccd
Noah Manneschmidt <noah@manneschmidt.net>
2016-12-17 03:40:02 -0800

    Add blurb

--- client-src/components/App.tsx
+++ client-src/components/App.tsx
@@ -15,6 +15,7 @@ export class App extends Component<AppProps, void> {
         return <div id="App">
             <UsersListContainer selectedUser={this.props.params.username} comparedUser={this.props.params.other_username} />
             <div id="contents">
+                <p>A friendly little competition: Predict Giant Bomb's top 10 games of 2016 better and win the admiration of your peers!</p>
                 {this.props.children}
             </div>
         </div>;
